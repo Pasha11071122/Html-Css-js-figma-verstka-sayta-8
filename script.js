3d54e469e764631ed64519477362ce317bd768a8
Pasha Novikov <111934225+Pasha11071122@users.noreply.github.com>
2023-06-11 00:33:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(function(){
+  $('.design-slider').slick({
+    dots: false,
+    slidesToShow: 4,
+    variableWidth: true,
+    prevArrow: '<img class="arrow arrow-left" src="img/Arrow 1.svg" alt="">',
+    nextArrow: '<img class="arrow arrow-right" src="img/Arrow 2.svg" alt="">',
+  });  
+});
